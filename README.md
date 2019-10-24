6e20c9f4e3383151db736d7fd2df96112fa8f693
Ale Bausa <39669342+alebausa@users.noreply.github.com>
2019-10-24 15:23:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Starter kit API Backend
+
+```javascript
+$ npm install
+$ npm run start:dev
+```
+
+El backend se ejecuta en el puerto 3001 por defecto
+
+**importante** cambiar el `.env-sample` a `.env` con vuestras variables de entorno.
---
+++ app.js
@@ -0,0 +1,76 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
+require('dotenv').config();
+
+mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to: ', process.env.MONGO_URL);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+const authRouter = require('./routes/auth');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  }),
+);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL],
+  }),
+);
+
+app.use((req, res, next) => {
+  app.locals.currentUser = req.session.currentUser;
+  next();
+});
+
+app.use('/', authRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({ code: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({ code: 'unexpected' });
+  }
+});
+
+module.exports = app;
