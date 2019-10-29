da28584bbc97baaf9aec388f6e1f2831406a98bc
Nomeolvides <info@nomeolvides.cat>
2019-10-29 18:57:49 +0100

    no changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,63 +1,86 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const { checkUsernameAndPasswordNotEmpty, checkIfLoggedIn } = require('../middlewares');
-const User = require('../models/User');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const {
+  checkUsernameAndPasswordNotEmpty,
+  checkIfLoggedIn
+} = require("../middlewares");
+const User = require("../models/User");
 
 const bcryptSalt = 10;
 const router = express.Router();
 
-router.get('/me', (req, res, next) => {
+router.get("/me", (req, res, next) => {
   if (req.session.currentUser) {
     res.status(200).json(req.session.currentUser);
   } else {
-    res.status(401).json({ code: 'unauthorized' });
+    res.status(401).json({ code: "unauthorized" });
   }
 });
 
-router.post('/signup', checkUsernameAndPasswordNotEmpty, async (req, res, next) => {
-  const {
-    username, password, email, isOver16, allowsLocation, allowsContact, darkMode, avatar,
-  } = res.locals.auth;
-  try {
-    const user = await User.findOne({ username });
-    if (user) {
-      return res.status(422).json({ code: 'username-not-unique' });
+router.post(
+  "/signup",
+  checkUsernameAndPasswordNotEmpty,
+  async (req, res, next) => {
+    const {
+      username,
+      password,
+      email,
+      isOver16,
+      allowsLocation,
+      allowsContact,
+      darkMode,
+      avatar
+    } = res.locals.auth;
+    try {
+      const user = await User.findOne({ username });
+      if (user) {
+        return res.status(422).json({ code: "username-not-unique" });
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      const newUser = await User.create({
+        username,
+        hashedPassword,
+        email,
+        isOver16,
+        allowsLocation,
+        allowsContact,
+        darkMode,
+        avatar
+      });
+      req.session.currentUser = newUser;
+      return res.json(newUser);
+    } catch (error) {
+      next(error);
     }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-
-    const newUser = await User.create(
-      {
-        username, hashedPassword, email, isOver16, allowsLocation, allowsContact, darkMode, avatar,
-      },
-    );
-    req.session.currentUser = newUser;
-    return res.json(newUser);
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-router.post('/login', checkUsernameAndPasswordNotEmpty, async (req, res, next) => {
-  const { email, password } = res.locals.auth;
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ code: 'not-found' });
+router.post(
+  "/login",
+  checkUsernameAndPasswordNotEmpty,
+  async (req, res, next) => {
+    const { email, password } = res.locals.auth;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ code: "not-found" });
+      }
+      if (bcrypt.compareSync(password, user.hashedPassword)) {
+        req.session.currentUser = user;
+        return res.json(user);
+      }
+      return res.status(404).json({ code: "not-found" });
+    } catch (error) {
+      next(error);
     }
-    if (bcrypt.compareSync(password, user.hashedPassword)) {
-      req.session.currentUser = user;
-      return res.json(user);
-    }
-    return res.status(404).json({ code: 'not-found' });
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-router.get('/logout', (req, res, next) => {
-  req.session.destroy((err) => {
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(err => {
     if (err) {
       next(err);
     }
@@ -65,7 +88,7 @@ router.get('/logout', (req, res, next) => {
   });
 });
 
-router.put('/edit', checkIfLoggedIn, async (req, res, next) => {
+router.put("/edit", checkIfLoggedIn, async (req, res, next) => {
   const {
     username,
     password,
@@ -74,7 +97,7 @@ router.put('/edit', checkIfLoggedIn, async (req, res, next) => {
     allowsLocation,
     allowsContact,
     darkMode,
-    avatar,
+    avatar
   } = req.body;
   const { _id } = req.session.currentUser;
   try {
@@ -86,7 +109,7 @@ router.put('/edit', checkIfLoggedIn, async (req, res, next) => {
       allowsLocation,
       allowsContact,
       darkMode,
-      avatar,
+      avatar
     });
     req.session.currentUser = user;
     return res.json(user);
