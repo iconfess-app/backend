6f477ec1d2b69b048ca3e7f30317062c89832b2f
Ale Bausa <ale.bausa@gmail.com>
2019-10-29 19:35:21 +0100

    testing 2

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
+const express = require('express');
+const bcrypt = require('bcrypt');
 const {
   checkUsernameAndPasswordNotEmpty,
   checkIfLoggedIn
-} = require("../middlewares");
-const User = require("../models/User");
+} = require('../middlewares');
+const User = require('../models/User');
 
 const bcryptSalt = 10;
 const router = express.Router();
 
-router.get("/me", (req, res, next) => {
+router.get('/me', (req, res, next) => {
   if (req.session.currentUser) {
     res.status(200).json(req.session.currentUser);
   } else {
-    res.status(401).json({ code: "unauthorized" });
+    res.status(401).json({ code: 'unauthorized' });
   }
 });
 
@@ -26,9 +26,8 @@ router.post('/signup', checkUsernameAndPasswordNotEmpty, async (req, res, next) 
     if (user) {
       return res.status(422).json({ code: 'username-not-unique' });
     }
-
-      const salt = bcrypt.genSaltSync(bcryptSalt);
-      const hashedPassword = bcrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
     const newUser = await User.create(
       {
@@ -40,31 +39,27 @@ router.post('/signup', checkUsernameAndPasswordNotEmpty, async (req, res, next) 
   } catch (error) {
     next(error);
   }
-);
+});
 
-router.post(
-  "/login",
-  checkUsernameAndPasswordNotEmpty,
-  async (req, res, next) => {
-    const { email, password } = res.locals.auth;
-    try {
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(404).json({ code: "not-found" });
-      }
-      if (bcrypt.compareSync(password, user.hashedPassword)) {
-        req.session.currentUser = user;
-        return res.json(user);
-      }
-      return res.status(404).json({ code: "not-found" });
-    } catch (error) {
-      next(error);
+router.post('/login', checkUsernameAndPasswordNotEmpty, async (req, res, next) => {
+  const { email, password } = res.locals.auth;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ code: 'not-found' });
     }
+    if (bcrypt.compareSync(password, user.hashedPassword)) {
+      req.session.currentUser = user;
+      return res.json(user);
+    }
+    return res.status(404).json({ code: 'not-found' });
+  } catch (error) {
+    next(error);
   }
-);
+});
 
-router.get("/logout", (req, res, next) => {
-  req.session.destroy(err => {
+router.get('/logout', (req, res, next) => {
+  req.session.destroy((err) => {
     if (err) {
       next(err);
     }
@@ -72,7 +67,7 @@ router.get("/logout", (req, res, next) => {
   });
 });
 
-router.put("/edit", checkIfLoggedIn, async (req, res, next) => {
+router.put('/edit', checkIfLoggedIn, async (req, res, next) => {
   const {
     username,
     password,
