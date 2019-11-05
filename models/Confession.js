d2398ae500b41b669565d28192df3f3bbe4490d5
Nomeolvides <info@nomeolvides.cat>
2019-11-05 07:15:04 +0100

    Updated confession model: categories

--- models/Confession.js
+++ models/Confession.js
@@ -1,25 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const confessionSchema = new Schema(
   {
     description: { type: String, required: true },
-    category: { type: String, required: true },
+    category: { type: Array, required: true },
     isDestroyed: { type: Boolean, default: true },
     date: { type: String },
     time: { type: String },
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
-    likesCounter: { type: Number, default: 0 },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    likesCounter: { type: Number, default: 0 }
   },
   {
     timestamps: {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-    },
-  },
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
 );
 
-const Confession = mongoose.model('Confession', confessionSchema);
+const Confession = mongoose.model("Confession", confessionSchema);
 
 module.exports = Confession;
