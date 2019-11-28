1b88e63422954d5230c6f1b307654830111b922a
Ale Bausa <ale.bausa@gmail.com>
2019-11-28 14:11:33 +0100

    changes headers order

--- app.js
+++ app.js
@@ -66,6 +66,12 @@ app.use('/', authRouter);
 app.use('/', confessionRouter);
 app.use('/', chatRouter);
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://iconfess.netlify.com/');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   res.status(404).json({ code: 'not found' });
@@ -79,12 +85,6 @@ app.use((err, req, res, next) => {
   if (!res.headersSent) {
     res.status(500).json({ code: 'unexpected' });
   }
-});
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://iconfess.netlify.com/');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
 });
 
 module.exports = app;
